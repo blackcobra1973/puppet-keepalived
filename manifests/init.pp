bbbaad3ac3b1f9f7455437d1f9784586fb5017f9
Kurt Dillen <kurt.dillen@dls-belgium.com>
2015-04-24 11:10:41 +0200

    * Remove params.pp -> migrated to init.pp. [SYS-537]

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,46 @@
 # == Class keepalived
 #
 class keepalived (
-  $config_dir         = $::keepalived::params::config_dir,
-  $config_dir_mode    = $::keepalived::params::config_dir_mode,
-  $config_file_mode   = $::keepalived::params::config_file_mode,
-  $config_group       = $::keepalived::params::config_group,
-  $config_owner       = $::keepalived::params::config_owner,
-  $daemon_group       = $::keepalived::params::daemon_group,
-  $daemon_user        = $::keepalived::params::daemon_user,
-  $pkg_ensure         = $::keepalived::params::pkg_ensure,
-  $pkg_list           = $::keepalived::params::pkg_list,
-  $service_enable     = $::keepalived::params::service_enable,
-  $service_ensure     = $::keepalived::params::service_ensure,
-  $service_hasrestart = $::keepalived::params::service_hasrestart,
-  $service_hasstatus  = $::keepalived::params::service_hasstatus,
-  $service_manage     = $::keepalived::params::service_manage,
-  $service_name       = $::keepalived::params::service_name,
-  $service_restart    = $::keepalived::params::service_restart,
-) inherits keepalived::params {
+  $config_dir         = '/etc/keepalived',
+  $config_dir_mode    = '0755',
+  $config_file_mode   = '0644',
+  $config_group       = 'root',
+  $config_owner       = 'root',
+  $daemon_group       = 'root',
+  $daemon_user        = 'root',
+  $pkg_ensure         = 'present',
+  $pkg_list           = [ 'keepalived' ],
+  $service_enable     = true,
+  $service_ensure     = 'running',
+  $service_manage     = true,
+#  $service_name       = 'keepalived',
+  $service_restart    = undef,
+)
+{
+  case $::osfamily {
+    'redhat': {
+      $service_hasstatus  = true
+      $service_hasrestart = true
+      $service_name       = 'keepalived'
+    }
+
+    'debian': {
+      $service_hasrestart = false
+      $service_hasstatus  = false
+      $service_name       = 'keepalived'
+    }
+
+    'gentoo': {
+      $service_hasrestart = false
+      $service_hasstatus  = false
+      $service_name       = 'keepalived'
+    }
+
+    default: {
+      fail "Operating system ${::operatingsystem} is not supported."
+    }
+  }
+
   validate_absolute_path($config_dir)
   validate_re($config_dir_mode, '^[0-9]+$')
   validate_re($config_file_mode, '^[0-9]+$')
@@ -29,9 +52,9 @@ class keepalived (
   validate_bool($service_manage)
   validate_string($service_name)
 
-  class { 'keepalived::install': } ->
-  class { 'keepalived::config': } ->
-  class { 'keepalived::service': } ->
-  Class[ 'keepalived' ]
+#  class { 'keepalived::install': } ->
+#  class { 'keepalived::config': } ->
+#  class { 'keepalived::service': } ->
+#  Class[ 'keepalived' ]
 }
 
